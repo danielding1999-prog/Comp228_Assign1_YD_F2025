318e418761401bf89ae1ca9c6dc5975b4dc8f4a0
Yiming <danielding1999@gmail.com>
2026-09-19 23:09:33 -0400

    Add author, date, and description to class CADToUSD

--- Assignment1_DanielDing_F2026/src/com/yd/assign1/CADToUSD.java
+++ Assignment1_DanielDing_F2026/src/com/yd/assign1/CADToUSD.java
@@ -1,7 +1,11 @@
 package com.yd.assign1;
-
 import java.util.Scanner;
-
+/*
+ * Author: Yiming(Daniel) Ding
+ * Date: 09/19/2026
+ * Description: Takes in 1 input from user which is CAD and convert it
+ * to USD
+ */
 public class CADToUSD {
 
 	public void Convert() {
